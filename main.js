2a9f7b29a6331507d85d444625b6b89f14bf8717
davidepatruno <patrunodavid@gmail.com>
2018-11-28 17:53:27 +0100

    blocco contatore risolto bug da risolvere : quadratino già premuto e contatore che aumenta

--- main.js
+++ main.js
@@ -3,17 +3,31 @@ var contatoreverde = 0;
 document.getElementById('rossi').innerHTML = contatorerosso;
 document.getElementById('verdi').innerHTML = contatoreverde;
 
+
 $('.quadrato').click(function() {
   var controllo = $(this).hasClass('r');
   console.log(controllo);
   if (controllo == true) {
-    $(this).css('background-color','red');
-    contatorerosso++;
-    document.getElementById('rossi').innerHTML = contatorerosso;
+      $(this).css('background-color','red');
+      contatorerosso++;
+      if (contatorerosso > 15) {
+        document.getElementById('rossi').innerHTML = "15";
+      }
+      else {
+        document.getElementById('rossi').innerHTML = contatorerosso;
+      }
   }
   else {
     $(this).css('background-color','green');
     contatoreverde++;
-    document.getElementById('verdi').innerHTML = contatoreverde;
+    if (contatoreverde > 49) {
+      document.getElementById('verdi').innerHTML = "49";
+    }
+    else {
+      document.getElementById('verdi').innerHTML = contatoreverde;
+    }
   }
+  if ((contatorerosso >= 15) && (contatoreverde >= 49)) {
+    alert('HAI COMPLETATO IL GIOCO');
+  };
 });
