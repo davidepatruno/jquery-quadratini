92df0a48dd791025b8f966e3da888751d341f7d4
davidepatruno <patrunodavid@gmail.com>
2018-11-28 16:54:24 +0100

    Completo

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+var contatorerosso = 0;
+var contatoreverde = 0;
+document.getElementById('rossi').innerHTML = contatorerosso;
+document.getElementById('verdi').innerHTML = contatoreverde;
+
 $('.quadrato').click(function() {
   var controllo = $(this).hasClass('r');
   console.log(controllo);
   if (controllo == true) {
     $(this).css('background-color','red');
+    contatorerosso++;
+    document.getElementById('rossi').innerHTML = contatorerosso;
   }
   else {
-  $(this).css('background-color','green');
+    $(this).css('background-color','green');
+    contatoreverde++;
+    document.getElementById('verdi').innerHTML = contatoreverde;
   }
 });
